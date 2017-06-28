f3a333b6f50328542fb67407c7e7706ba4dcd89a
Franklin <franklid@ds.ad.adp.com>
2017-06-27 22:52:26 -0700

    finished lesson 6

--- main.py
+++ main.py
@@ -17,6 +17,22 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -28,15 +44,18 @@ def index():
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.all()   
-    return render_template('todos.html', title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()  
+    return render_template('todos.html', title="Get It Done!", 
+    tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route("/delete-task", methods=["POST"])
 def delete_task():
 
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
     return redirect('/')
