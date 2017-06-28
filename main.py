07166ffe770cce47eb42ee57ed0653f5dcb385bf
Franklin <franklid@ds.ad.adp.com>
2017-06-28 07:12:10 -0700

    completed all get-it-done lessons

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
         self.completed = False
+        self.owner = owner
+
 
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -29,27 +34,31 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
+
     allowed_routes = ['login', 'register']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email).first()
+        
         if user and user.password == password:
             session['email'] = email
             flash("Logged in")
             return redirect('/')
         else:
-            #TODO - explain why login failed
-            return '<h1>Error!</h1>'
+            flash('User password incorrect, or user does not exist', 'error')
+            
     return render_template('login.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -57,6 +66,7 @@ def register():
 
         #TODO - validate user's data
         existing_user = User.query.filter_by(email=email).first()
+        
         if not existing_user:
             new_user = User(email, password)
             db.session.add(new_user)
@@ -77,14 +87,16 @@ def logout():
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()  
+    tasks = Task.query.filter_by(completed=False,owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()  
     return render_template('todos.html', title="Get It Done!", 
     tasks=tasks, completed_tasks=completed_tasks)
 
